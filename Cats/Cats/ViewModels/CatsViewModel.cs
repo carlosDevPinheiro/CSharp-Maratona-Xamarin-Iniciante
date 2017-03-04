8b04a364d23c820a18a6362cdd917b6bf3db902d
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2017-03-04 15:01:30 -0400

    Finalizado o modulo 4, tudo funcioanando Perfeitamente

---
+++ Cats/Cats/ViewModels/CatsViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Cats.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cats.ViewModels
+{
+    public class CatsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Cat> Cats { get; set; }
+        public Command GetCatsCommand { get; set; }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private bool _Busy;
+
+        public CatsViewModel()
+        {
+            Cats = new ObservableCollection<Cat>();
+            GetCatsCommand = new Command(async () =>  await GetCats(), () => !IsBusy );
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _Busy;
+            }
+
+            set
+            {
+                _Busy = value; OnPropertyChanged(); GetCatsCommand.ChangeCanExecute();
+            }
+        }
+
+        async Task GetCats()
+        {
+            if (!IsBusy)
+            {
+                Exception Error = null;
+
+                try
+                {
+                    IsBusy = true;
+                    var Repository = new Repository();
+                    var Items = await Repository.GetCats();
+
+                    Cats.Clear();
+                    foreach (var Cat in Items)
+                    {
+                        Cats.Add(Cat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error = ex;                   
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+
+                if (Error != null)
+                {
+                    await Xamarin.Forms.Application.Current.MainPage.DisplayAlert(
+                    "Error!", Error.Message, "OK");
+                }
+            }
+            return;
+
+        }
+    }
+}
---
+++ Cats/Cats/Views/DetailsPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cats.Views
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+
+        Models.Cat SelectedCat;
+
+        public DetailsPage(Models.Cat selectedCat)
+        {
+            InitializeComponent();
+
+            this.SelectedCat = selectedCat;
+            BindingContext = this.SelectedCat;
+
+            ButtonWebSite.Clicked += ButtonWebSite_Clicked;
+        }
+
+        private void ButtonWebSite_Clicked(object sender, EventArgs e)
+        {
+            if (SelectedCat.WebSite.StartsWith("http"))
+            {                
+                Device.OpenUri(new Uri(SelectedCat.WebSite));
+            }            
+        }
+    }
+
+    //class DetailsPageViewModel : INotifyPropertyChanged
+    //{
+
+    //    public DetailsPageViewModel()
+    //    {
+    //        IncreaseCountCommand = new Command(IncreaseCount);
+    //    }
+
+    //    int count;
+
+    //    string countDisplay = "You clicked 0 times.";
+    //    public string CountDisplay
+    //    {
+    //        get { return countDisplay; }
+    //        set { countDisplay = value; OnPropertyChanged(); }
+    //    }
+
+    //    public ICommand IncreaseCountCommand { get; }
+
+    //    void IncreaseCount() =>
+    //        CountDisplay = $"You clicked {++count} times";
+
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    // }
+}
